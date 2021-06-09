42802c36417edfa7f9bf4542e937ceab8eaba1b7
Amanzhan <amanzhan.kakim@nu.edu.kz>
2021-06-09 12:24:24 +0500

    arguments were placed wrong

--- api/graphql/records/DAL.js
+++ api/graphql/records/DAL.js
@@ -1,45 +1,65 @@
 module.exports = {
   findRecordByName: async (name, id, db) => {
-    const obj = await db.query(
-      "SELECT * FROM records WHERE username = $1 AND recordid = $2",
-      [name, id]
-    );
+    try {
+      const obj = await db.query(
+        "SELECT * FROM records WHERE username = $1 AND recordid = $2",
+        [name, id]
+      );
 
-    return obj.rows;
+      return obj.rows;
+    } catch (err) {
+      throw err;
+    }
   },
 
   findAllRecords: async (id, db) => {
-    const obj = await db.query("SELECT * FROM records WHERE recordid = $1", [
-      id,
-    ]);
+    try {
+      const obj = await db.query("SELECT * FROM records WHERE recordid = $1", [
+        id,
+      ]);
 
-    return obj.rows;
+      return obj.rows;
+    } catch (err) {
+      throw err;
+    }
   },
 
   insertRecord: async (name, password, id, db) => {
-    const obj = await db.query(
-      "INSERT INTO records (username, password, recordid) values ($1, $2, $3) RETURNING *",
-      [name, password, id]
-    );
+    try {
+      const obj = await db.query(
+        "INSERT INTO records (username, password, recordid) values ($1, $2, $3) RETURNING *",
+        [name, password, id]
+      );
 
-    return obj.rows;
+      return obj.rows;
+    } catch (err) {
+      throw err;
+    }
   },
 
   deleteRecord: async (id, db) => {
-    const obj = await db.query(
-      "DELETE FROM records WHERE serialid = $1 RETURNING *",
-      [id]
-    );
+    try {
+      const obj = await db.query(
+        "DELETE FROM records WHERE serialid = $1 RETURNING *",
+        [id]
+      );
 
-    return obj.rows[0];
+      return obj.rows[0];
+    } catch (err) {
+      throw err;
+    }
   },
 
-  updateRecord: async (name, password, id, db) => {
-    const obj = await db.query(
-      "UPDATE records SET username = $1, password = $2 WHERE serialid = $3 RETURNING *",
-      [name, password, id]
-    );
+  updateRecord: async (id, name, password, db) => {
+    try {
+      const obj = await db.query(
+        "UPDATE records SET username = $1, password = $2 WHERE serialid = $3 RETURNING *",
+        [name, password, id]
+      );
 
-    return obj.rows[0];
+      return obj.rows[0];
+    } catch (err) {
+      throw err;
+    }
   },
 };
